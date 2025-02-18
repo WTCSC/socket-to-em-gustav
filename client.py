f6572173b06596517e59bf12cf93c824f15554a1
GustavNord <gus.nordqvist@gmail.com>
2025-02-18 15:02:03 -0700

    started client

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(('localhost, 5000'))
+print("Connected to server")
+
+while True:
+    msg = input("Enter message: ")
+    if not msg:
+      break
+    client.send(msg.encode())
+    response = client.recv(1024).decode()
+    print(f"Server says: {response}")
+
+client.close()
