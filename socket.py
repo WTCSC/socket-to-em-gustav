fca836e45bcb4e078ecedbe4d384e3f0b62d94fc
GustavNord <gus.nordqvist@gmail.com>
2025-02-18 14:56:33 -0700

    added server

---
+++ socket.py
@@ -0,0 +1,21 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind(('0.0.0.0', 5000))
+
+server.listen(6)
+print("Waiting for connection...")
+
+client, addr = server.accept()
+print(f"Connected to {addr}")
+
+while True:
+    msg = client.recv(1024).decode()
+    if not msg:
+        break
+    print(f"Recieved: {msg}")
+    client.send(f"server received: {msg}".encode())
+
+client.close()
+server.close()
